e88dc6d45a0d31a5ed16c01509386c66bdb674fb
zodieth <contact.prodzodi@gmail.com>
2023-01-18 12:23:12 -0300

    merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,46 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { RouterProvider } from 'react-router-dom';
-import {router} from './router/index'
-import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink, gql } from '@apollo/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/index";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const container = document.getElementById('root')!;
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+  gql,
+} from "@apollo/client";
+
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 const getAuth = () => {
-  const token = localStorage.getItem('user-token')
-  return token ? token : null
-}
+  const token = localStorage.getItem("user-token");
+  return token ? token : null;
+};
 
 const client = new ApolloClient({
   connectToDevTools: true,
-  cache: new InMemoryCache,
+  cache: new InMemoryCache(),
   link: new HttpLink({
     headers: {
-      authorization: getAuth()
+      authorization: getAuth(),
     },
-    uri: 'http://localhost:4000'
-  })
-})
+    uri: "http://localhost:4000",
+  }),
+});
 
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <RouterProvider router={router} /> 
+        <ChakraProvider>
+          <RouterProvider router={router} />
+        </ChakraProvider>
       </Provider>
     </ApolloProvider>
   </React.StrictMode>
 );
-
