2b958e24e6398a1b7736bb7fbedc7909fb6ae1ef
Puchoanimal <santiagovera280@gmail.com>
2023-01-19 11:56:07 -0300

    carrusel y footer

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,16 @@
 import NavBar from "../components/NavBar/NavBar";
 import style from "../app.module.css";
 import SubNav from "../components/NavBar/SubNav";
+import Footer from "../components/Footer/Footer";
+import FooterTwo from "../components/FooterTwo/FooterTwo";
 
 function Home() {
   return (
     <div className={style.app}>
       <NavBar />
       <SubNav />
+      <FooterTwo />
+      <Footer />
     </div>
   );
 }
