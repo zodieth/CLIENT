3e444fa3227e10dd0ac91337f7282d0124f5e11a
zodieth <contact.prodzodi@gmail.com>
2023-01-24 20:19:12 -0300

    Delete from cart action

--- src/features/ActionTypes.ts
+++ src/features/ActionTypes.ts
@@ -3,7 +3,9 @@ export const PRODUCT_LOADING = "PRODUCT_LOADING";
 export const PRODUCT_FAILED = "PRODUCT_FAILED";
 export const PRODUCT_FIND = "PRODUCT_FIND";
 export const PRODUCT_FILTER = "PRODUCT_FILTER";
+
 export const ADD_TO_CART = "ADD_TO_CART";
+export const DELETE_FROM_CART = "DELETE_FROM_CART";
 
 export const BRAND_ADD = "BRAND_ADD";
 export const BRAND_LOADING = "BRAND_LOADING";
--- src/features/cart/Cart.ts
+++ src/features/cart/Cart.ts
@@ -1,5 +1,19 @@
 import * as ActionTypes from "../ActionTypes";
 
+type Product = {
+  name: String;
+  description: String;
+  price: Number;
+  images: [String];
+  category: {
+    name: String;
+  };
+  brand: {
+    name: String;
+  };
+  active: Boolean;
+};
+
 export const Cart = (
   state = {
     cart: [],
@@ -11,6 +25,13 @@ export const Cart = (
       return {
         ...state,
         cart: state.cart.concat(action.payload),
+      };
+    case ActionTypes.DELETE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter(
+          (product: Product) => product.name !== action.payload.name
+        ),
       };
 
     default:
