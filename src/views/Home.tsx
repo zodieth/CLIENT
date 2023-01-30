1fdd6f2ed01ae490f4abeeb7e2fcd92679415d1c
Juan Leiton <jgleitonl@gmail.com>
2023-01-30 09:18:08 -0500

    Login, logout y signup.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -10,11 +10,40 @@ import { fetchProductsApi, productsFilter, fetchBrandApi, fetchCategoryApi } fro
 import CarouselDown from "../components/CarouselDown/CarouselDown";
 import Cards from "../components/ProductCards/Cards";
 import Filter from "../components/Filtro/filter";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Home() {
   const dispatch = useAppDispatch();
-  
+  const { loginWithRedirect, getAccessTokenSilently, user, isAuthenticated, isLoading, error } = useAuth0();
+
+  useEffect(() => {
+    console.log("My user: ", user);
+    console.log("Authenticated: ", isAuthenticated);
+    console.log("Loading: ", isLoading);
+    console.log("Error: ", error);
+  }, [user, isAuthenticated, isLoading, error]);
+
+  useEffect(() => {
+    if(isAuthenticated) {
+      // loginWithRedirect();
+      const getToken = async () => {
+      const accessToken = await getAccessTokenSilently();
+      console.log("Token: ", accessToken);
+      const response = await fetch("http://localhost:3001/claims", { //Ejemplo real...
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${accessToken}`
+        },
+      });
+      const allClaims = await response.json();
+      console.log("Claims: ", allClaims);
+      return allClaims;
+    };
+    getToken();
+    };
+  }, [getAccessTokenSilently, isAuthenticated]);
+
   useEffect(() => {
     dispatch(fetchProductsApi());
     dispatch(fetchBrandApi());
