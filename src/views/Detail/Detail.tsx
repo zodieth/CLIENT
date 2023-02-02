ec063255b01635b6febb94d3dfac2d92d19795f7
LucasParra95 <lucasandresparra@gmail.com>
2023-02-02 11:03:30 -0300

    Vista detail

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -17,7 +17,7 @@ import { addToCart, deleteFromCart } from "../../app/actionsCreators";
 import Swal from "sweetalert2";
 import NuevoCarrusel from "./NuevoCarrusel";
 
-function Detail() {
+function Detail(props: any) {
   const { name } = useParams();
   const products = useAppSelector((state: any) => state.products);
 
@@ -75,6 +75,24 @@ function Detail() {
     });
   };
 
+  //////////////////////////////
+
+  type review = {
+    review: number;
+  };
+
+  const startPercentage = () => {
+    let total = 100;
+    props.reviews?.forEach(function (a: review) {
+      total += a.review;
+    });
+    const percentage = total / props.reviews?.length;
+    const starPercentage = ((percentage ? percentage : 0 / 100) / 5) * 100;
+
+    return starPercentage;
+  };
+  ///////////////////////////////////////////
+
   return (
     <div>
       <div className={style.navBar}>
@@ -89,14 +107,13 @@ function Detail() {
             <div className={style.detail} key={e}>
               <div className={style.contenedor}>
                 <div>
+                  <div className={style.name}>{e.name}</div>
                   <img
                     src={e.images[0]}
                     alt="imgDetail"
                     className={style.imgDetail}
                   />
-                </div>
-                <div className={style.right}>
-                  <div className={style.name}>{e.name}</div>
+                  <h4 className={style.texto}>* Las imágenes se exhiben con fines ilustrativos.</h4>
                   <div className={style.price_cart}>
                     <div className={style.price}>US$ {e.price}</div>
                     <Button
@@ -116,9 +133,37 @@ function Detail() {
                       )}
                     </Button>
                   </div>
+                  <div className={style.calificacion}>
+                    <div className={style.starsOuter}>
+                      <div
+                        className={style.starsInner}
+                        style={{ width: `${startPercentage()}%` }}
+                      ></div>
+                    </div>
+                  </div>
                 </div>
-                <div className={style.texto}>
+
+                <div className={style.right}>
+                  <div className={style.description}>{e.description}</div>
+
+                  
+                </div>
+                {/* <div className={style.texto}>
                   <h4>* Las imágenes se exhiben con fines ilustrativos.</h4>
+                </div> */}
+              </div>
+
+              <div className={style.contenedor}>
+                <div>
+                  <div className={style.description}>Preguntas de nuestros clientes</div>
+                  {e.questions.map((q:any)=>{
+                    console.log(q)
+                  })}
+                </div>
+              </div>
+              <div className={style.contenedor}>
+                <div>
+                  <div className={style.description}>Haznos una pregunta:</div>
                 </div>
               </div>
             </div>
